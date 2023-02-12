19ffd429ba97056843248de08fd0500f556cdbec
Ariel Debrouvier <ariel.debrouvier@gmail.com>
2023-02-12 03:08:20 -0300

    Add rubocop (#17)

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -6,9 +6,10 @@
 require 'jekyll/tagging'
 require 'sentry-ruby'
 
-Jekyll::Hooks.register :site, :after_init do |excerpt|
-    Sentry.init do |config|
-        config.dsn = ENV['SENTRY_DSN']
-        config.traces_sample_rate = 0.5
-    end
+Jekyll::Hooks.register :site, :after_init do |_excerpt|
+  Sentry.init do |config|
+    config.dsn = ENV['SENTRY_DSN']
+    config.environment = ENV['JEKYLL_ENV']
+    config.traces_sample_rate = 0.5
+  end
 end
