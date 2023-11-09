a0123613c24aeb653719f0067650902d7a6fe634
Ariel Debrouvier <ariel.debrouvier@gmail.com>
2023-11-09 00:16:43 -0300

    Fix tina config

--- tina/config.ts
+++ tina/config.ts
@@ -11,6 +11,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
+  token: process.env.TINA_TOKEN,
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID,
   client: { skip: true },
   build: {
     outputFolder: "admin",
